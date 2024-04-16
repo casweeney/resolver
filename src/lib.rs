c2be5f2be39cb959e3125c438cad88b2d35ca2bf
casweeney <casweeno2000@gmail.com>
2024-04-16 08:00:08 +0100

    WIP: implemented initialization function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+pub const GIT_HARDHAT_JS_URL: &str = "https://github.com/mudgen/diamond-3-hardhat.git";
+pub const GIT_HARDHAT_TS_URL: &str = "https://github.com/Timidan/diamond-3-hardhat-typechain.git";
+pub const GIT_FOUNDRY_URL: &str = "https://github.com/FydeTreasury/Diamond-Foundry.git";
+
 pub enum Command {
     Scaffold,
     Get
 }
 
-pub enum ScaffoldItem {
+pub enum Item {
+    HardhatJavascript, // hhjs
+    HardhatTypescript, // hhts
+    Foundry, // fd
     Hardhat,
     React,
     Next,
@@ -11,19 +18,13 @@ pub enum ScaffoldItem {
     Laravel
 }
 
-pub enum GetItem {
-    HardhatJavascript, // hhjs
-    HardhatTypescript, // hhts
-    Foundry // fd
-}
-
 pub struct Config {
-    pub command: String,
-    pub item: String,
+    pub command: Command,
+    pub item: Item,
 }
 
 impl Config {
-    pub fn new(args: Vec<String>) -> Result<Config, &'static str> {
+    pub fn init(args: Vec<String>) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Incomplete arguments");
         }
@@ -35,9 +36,31 @@ impl Config {
             return Err("Invalid command");
         }
 
-        Ok(Config {
-            command,
-            item
-        })
+        if item.is_empty() {
+            return Err("Invalid item name");
+        }
+
+        match command.as_str() {
+            "get" => {
+                match item.as_str() {
+                    "hhjs" => Ok(Config {command: Command::Get, item: Item::HardhatJavascript}),
+                    "hhts" => Ok(Config {command: Command::Get, item: Item::HardhatTypescript}),
+                    "fd" => Ok(Config {command: Command::Get, item: Item::Foundry}),
+                    _ => {
+                        return Err("Wrong item name");
+                    }
+                }
+            },
+            "scaffold" => {
+                return Err("Use only get command");
+            }
+            _ => {
+                return Err("Use only get command");
+            }
+        }
+    }
+
+    pub fn resolve(config: &Config) {
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,11 @@ use resolver::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = match Config::new(args) {
+    let config = match Config::init(args) {
         Ok(cfg) => cfg,
         Err(e) => {
             eprintln!("Error initializing configuration: {}", e);
             return;
         }
     };
-
-    println!("Command: {}, Argument: {}", config.command, config.item);
 }
